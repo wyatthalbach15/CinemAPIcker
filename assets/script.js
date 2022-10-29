98dc4ab8fa944a567ce375267ebf431a1b408950
Zack Levin <zacklevin@gmail.com>
2022-10-29 16:37:44 -0700

    getMovieData, addMovieData, and SelectButton
    added functions for movie data display and streaming data display

--- assets/script.js
+++ assets/script.js
@@ -85,3 +85,77 @@ function getStreamingData(movieId) {
 }
 
 // getStreamingData("tt0167260");
+
+
+// Gets movie data
+function getMovieData(movieId) {
+    var imdbApiUrl = "https://imdb-api.com/en/API/Title/k_zns86b2w/" + movieId;
+
+    fetch(imdbApiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log("--IMDb--");
+                console.log(data);
+                console.log(data.fullTitle);
+
+                let movieObject = {
+                    title: data.fullTitle,
+                    year: data.year,
+                    rating: data.imDbRating,
+                    image: data.image,
+                    plot: data.plot,
+                    streaming: getStreamingData(data.id)
+                };
+
+                console.log(movieObject);
+
+                return;
+            });
+        }
+    });
+}
+
+// add movie data to "movie-container"
+function addMovieData(movieObject) {
+    let movieContainer = document.querySelector("#movie-container");
+    let movieCard = document.createElement("div");
+    let movieImage = document.createElement("img");
+    let movieTitle = document.createElement("h2");
+    let movieYear = document.createElement("p");
+    let movieRating = document.createElement("p");
+    let moviePlot = document.createElement("p");
+    let movieStreaming = document.createElement("p");
+
+    movieCard.classList.add("movie-card");
+    movieImage.classList.add("movie-image");
+    movieTitle.classList.add("movie-title");
+    movieYear.classList.add("movie-year");
+    movieRating.classList.add("movie-rating");
+    moviePlot.classList.add("movie-plot");
+    movieStreaming.classList.add("movie-streaming");
+
+    movieImage.src = movieObject.image;
+    movieTitle.textContent = movieObject.title;
+    movieYear.textContent = movieObject.year;
+    movieRating.textContent = movieObject.rating;
+    moviePlot.textContent = movieObject.plot;
+    movieStreaming.textContent = movieObject.streaming;
+
+    movieCard.appendChild(movieImage);
+    movieCard.appendChild(movieTitle);
+    movieCard.appendChild(movieYear);
+    movieCard.appendChild(movieRating);
+    movieCard.appendChild(moviePlot);
+    movieCard.appendChild(movieStreaming);
+
+    movieContainer.appendChild(movieCard);
+}
+
+// Add event listener to “Select” button for each movie (for retrieve streaming data function)
+function SelectButton(movieId) {
+    let selectButton = document.querySelector("#select-button-" + movieId);
+    selectButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        getStreamingData(movieId);
+    });
+}
